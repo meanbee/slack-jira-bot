121c85dc1c103e540bc656552f7e6d9ccfc5ceb8
Nick Jones <nick@nicksays.co.uk>
2017-05-04 12:17:14 +0100

    Tidy up

--- src/github.com/meanbee/slack-jira-bot/jira-bot.go
+++ src/github.com/meanbee/slack-jira-bot/jira-bot.go
@@ -66,11 +66,11 @@ func handleIncomingMessage(message slack.Msg) {
 }
 
 func respondToIssueMentioned(channel string, issueID string) {
-	// defer func() {
-	// 	if e := recover(); e != nil {
-	// 		log.Printf("Exception responding to issue %s: %v", issueID, e)
-	// 	}
-	// }()
+	 defer func() {
+		if e := recover(); e != nil {
+			log.Printf("Exception responding to issue %s: %v", issueID, e)
+		}
+	 }()
 
 	api := getSlackAPI()
 
@@ -80,8 +80,6 @@ func respondToIssueMentioned(channel string, issueID string) {
 	}
 
 	issueData := getJiraIssue(issueID)
-
-	log.Printf("Issue fetched: %v", issueData)
 
 	api.PostMessage(channel, formatMessage(issueData), params)
 }
@@ -97,10 +95,6 @@ func getChannel(channelID string) (*slack.Channel, error) {
 }
 
 func formatMessage(issue gojira.Issue) string {
-	log.Printf(issue.Key)
-	log.Printf(issue.Fields.Summary)
-	log.Printf(issue.Fields.Reporter.DisplayName)
-
 	message := fmt.Sprintf(
 		"*%s: %s* _Reported by %s_ - %s",
 		issue.Key,
@@ -117,8 +111,6 @@ func getJiraURL(issueKey string) string {
 }
 
 func getJiraIssue(issueID string) gojira.Issue {
-	log.Printf("getJiraIssue")
-
 	jiraAPIPath := "/rest/api/2"
 	jiraActivityPath := "/activity"
 
